f339efe0330910accf04136c45bcdbd23e71b0c9
Hyacinthe Hamon <hamon.hyacinthe@gmail.com>
2019-08-07 15:13:28 +0200

    first commit

--- App.js
+++ App.js
@@ -1,236 +1,73 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text,
-  View,
-  StyleSheet,
-  PixelRatio,
-  TouchableHighlight,
-  Button,
-  Image,
-  Dimensions,
-  TextInput,
-} from 'react-native';
-
-import {
-  ViroSceneNavigator,
-  ViroARSceneNavigator
-} from 'react-viro';
-
-import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
-
-import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT, prototype } from 'react-native-maps';
-
-import Geolocation from '@react-native-community/geolocation';
-
-// Calculate map zoom
-const screen = Dimensions.get('window');
-const ASPECT_RATIO = screen.width / screen.height;
-const LATITUDE_DELTA = 0.001;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-
-
-
-// Dummy data
-const exampleMarker = [{
-    latlng: { latitude: 48.8983508, longitude: 2.3778904},
-    title: 'My Tesla car',
-    description: '427m',
+import React from 'react';
+import {PermissionsAndroid, Platform} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import _ from 'lodash';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import ArScreen from './screens/ArScreen';
+import Geolocation from '@react-native-community/geolocation'
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Map: MapScreen,
+    Ar: ArScreen,
   },
-];
-
-// Dummy id for example map markers
-var id = 0;
-
-/*
- react-viro AR API key
- */
-var sharedProps = {
-  apiKey:"48F904D3-E6A6-4D2F-B66A-FFBEC0CA4B69",
+  {
+    initialRouteName: 'Home',
+  }
+);
+export const requestPermissions = async () => {
+	if(Platform.OS === 'android') { 
+			const cameraResult = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA)
+			const gpsResult = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+			if(cameraResult === PermissionsAndroid.RESULTS.GRANTED && gpsResult === PermissionsAndroid.RESULTS.GRANTED){
+				cameraResult === true;
+				gpsResult === true;
+			}
+	}
+	return true;
 }
 
-var InitialARScene = require('./ARScene');
+async function requestGpsPermission() {
+	try {
+	  const granted = await PermissionsAndroid.request(
+		  
+		  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+		  {
+			title: 'GPS ACCESS',
+			message:
+				'Ask permission for GPS access',
+			buttonNeutral: 'Ask Me Later',
+			buttonNegative: 'Cancel',
+			buttonPositive: 'OK',
+		  },
+	  );
+	  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+		console.log('testAnything = ********* You can use the GPS *********');
+	  } else {
+		console.log('testAnything = GPS permission denied');
+	  }
+	} catch (err) {
+	  console.warn(err);
+	}
+  }
+  function onPress(){
+	watchId = Geolocation.watchPosition(
+		(position) => console.log("testAnything =  POSICION  OBTENIDA ",position.coords.latitude,position.coords.longitude)),
+		(err) => console.log("testAnything =  Errror obteniendo posicion GPS" + err);
+	console.log("testAnything = watchID :  "+watchId);
+}
+const AppContainer = createAppContainer(RootStack);
 
-export default class ViroSample extends React.Component {
+export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+		//requestGpsPermission();
+		//requestCameraPermission();
+		requestPermissions();
+		
+  }
   render() {
-    return (
-      <AppNavigation style={ styles.tabBar }/>
-    );
+    return <AppContainer />;
   }
 }
-
-class ARScreen extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      sharedProps: sharedProps,
-    }
-  }
-  static navigationOptions = {
-    tabBarLabel: 'AR',
-    tabBarIcon: ({ tintColor }) => (
-      <Image source={require('./icons/camera.png')
-      }
-      style={[styles.icon, {tintColor: tintColor}]} 
-      />
-    ),
-  };
-  render() {
-    return (
-      //husk API key
-      <ViroARSceneNavigator {...this.state.sharedProps}
-      initialScene={{scene: InitialARScene}} />
-    );
-  }
-}
-
-class GoogleMapsScreen extends React.Component {
-
-  static navigationOptions = {
-    tabBarLabel: 'Map',
-    tabBarIcon: ({ tintColor }) => (
-      <Image source={require('./icons/placeholder.png')
-      }
-      style={[styles.icon, {tintColor: tintColor}]} 
-      />
-    ),
-  };
-
-  constructor(){
-    super();
-    this.state = {
-      markers: exampleMarker,
-      region: {
-        latitude: 48.8983508,
-        longitude: 2.3778904,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-      currentPosition: {
-        latitude: 0.0,
-        longitude: 0.0,
-      }
-    }
-  }
-
-  componentDidMount() {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          currentPosition: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          }
-        });
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-  }
-  
-  render() {
-    return (
-      <View style={styles.mainView}>
-        <MapView
-        provider={ PROVIDER_DEFAULT }
-        style={ styles.container }
-        region={{ latitude: this.state.currentPosition.latitude,
-                  longitude: this.state.currentPosition.longitude,
-                  latitudeDelta: LATITUDE_DELTA,
-                  longitudeDelta: LONGITUDE_DELTA,
-                  }}
-        mapType="standard"
-        showsUserLocation={true}
-        userLocationAnnotationTitle="My position"
-        followsUserLocation={true}
-        showsMyLocationButton={true}
-        showsPointsOfInterest={true}
-        showsCompass={true}
-        showsIndoors={true}
-        zoomEnabled={true}
-        zoomControlEnabled={true}
-        loadingEnabled={true}
-        scrollEnabled={true}
-        >
-        {this.state.markers.map(marker => (
-        <MapView.Marker
-          coordinate={marker.latlng}
-          title={marker.title}
-          description={marker.description}
-          key={id++}
-        />
-        ))}
-        </MapView>
-        <View style={styles.searchBarContainer}>
-            <TextInput
-              placeholder=" Search..."
-              style={styles.searchBar}
-            />
-        </View>
-      </View>
-    );
-  }
-}
-
-const AppNavigator = createBottomTabNavigator({
-  Google: { screen: GoogleMapsScreen },
-  AR: { screen: ARScreen },
-},
-{
-  tabBarPosition: 'bottom',
-  animationEnabled: true,
-  tabBarOptions: {
-    activeTintColor: '#e91e63',
-  },
-});
-
-const AppNavigation = createAppContainer(AppNavigator);
-
-const styles = StyleSheet.create({
-  icon: {
-    width: 26,
-    height: 26,
-  },
-  ar: {
-    flex: 1,
-  },
-  mainView: {
-    position: 'absolute',
-    height: '100%',
-    width: '100%',
-  },
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between'
-  },
-  searchBar: {
-    elevation: 1,
-    width: '99%',
-    marginTop: 'auto',
-    marginBottom: 'auto',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-  searchBarContainer: {
-    elevation: 1,
-    backgroundColor: 'white',
-    width: '90%',
-    height: '6%',
-    marginLeft: '5%',
-    top: 40,
-    borderRadius: 3,
-    shadowOpacity: 0.75,
-    shadowRadius: 1,
-    shadowColor: 'gray',
-    shadowOffset: { height: 0, width: 0},
-  },
-});
-
-AppRegistry.registerComponent('prototype', () => prototype);
-module.exports = ViroSample
